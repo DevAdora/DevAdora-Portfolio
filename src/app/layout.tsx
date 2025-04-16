e897592d749e7610ec25d86465e3ab3db317aafb
Rai <2r.reiyhes@gmail.com>
2025-04-16 11:47:08 +0800

    Removed ununsed variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {Karla, Rubik} from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "DevAdora",
   description: "Personal Portfolio",
