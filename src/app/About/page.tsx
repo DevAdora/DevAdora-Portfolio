36d76b137a90b32619bc298850ea8b7e58738dcc
Rai M. Reyes Jr. <2r.reiyhes@gmail.com>
2025-08-05 21:37:47 +0800

    remove unused variables

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { motion } from "framer-motion";
 import Image from "next/image";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { SiMedium } from "react-icons/si";
